6af84cc2b690877684cb90c5c1cb1359ea441bec
Rodrigo Echaide <rodrigoechaide@gmail.com>
2019-08-29 17:23:58 -0300

    Add conditional to avoid to create unnecessary stages for libraries projects

--- vars/genericPipeline.groovy
+++ vars/genericPipeline.groovy
@@ -1,59 +1,128 @@
 def call(String projectType = "Library") {
 
+if (projectType != "Library") {
+
 	pipeline {
 
 		agent any
 
 		stages {
+
 			stage('Build') {
+				
 				steps {
+
 				sh "make build"
 				echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
-				}
-			}
-			stage('Test') {
-				steps {
-				sh "make test"
+				
 				}
 			}
 
-		if (projectType != "Library") {
+			stage('Test') {
+				
+				steps {
+				
+				sh "make test"
+				
+				}
+			}
 
 			stage('Build-Image') {
+				
 				when {
+				
 					branch 'production'
+				
 				}
+				
 				steps {
+				
 				sh "make build-image"
+				
 				}
 			}
+
 			stage('Push-Image') {
 				when {
+				
 					branch 'production'
+				
 				}
+				
 				steps {
+				
 				sh "make push-image"
+				
 				}
 			}
 
 			stage('Deploy') {
+				
 				steps {
+				
 				sh "make deploy"
+				
 				}
 			}
 			
 			stage('Only Development Stage') {
+				
 				when {
+				
 					branch 'development'
+				
 				}
+
 				steps {
+
 					echo "Only Development Stage"
+				
 				}
 			}
 
 		}
+	}
+
+	} else {
+
+	pipeline {
+
+		agent any
+
+		stages {
+
+			stage('Build') {
+				
+				steps {
+				
+				sh "make build"
+				echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
+				
+				}
+			}
+
+			stage('Test') {
+				
+				steps {
+				
+				sh "make test"
+				
+				}
+			}
+
+			stage('Upload') {
+
+				steps {
+
+				sh "make upload"
+				
+				}
+			}
+
+			}
 
 		}
 	}
 
 }
+
