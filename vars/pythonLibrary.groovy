91dbc822aae1abdf392503f5213a7e76e9bd69af
Rodrigo Echaide <rodrigoechaide@gmail.com>
2019-09-12 16:22:23 -0300

    Fixed NEXT_DEV_VERSION param name

--- vars/pythonLibrary.groovy
+++ vars/pythonLibrary.groovy
@@ -14,7 +14,7 @@ def call(Map pipelineParams) {
 		parameters {
 	    	booleanParam(name: 'RELEASE', defaultValue: false, description: 'Are you doing a Release?')
 	    	string(name: 'RELEASE_VERSION', defaultValue: '', description: 'Version to be released (e.g: 1.1.0, 1.1.0rc1)')
-	    	string(name: 'NEX_DEV_VERSION', defaultValue: '', description: 'Next development version (e.g: 1.2.0.dev1) according to <a href="https://www.python.org/dev/peps/pep-0440/">PEP-440</a>')
+	    	string(name: 'NEXT_DEV_VERSION', defaultValue: '', description: 'Next development version (e.g: 1.2.0.dev1) according to <a href="https://www.python.org/dev/peps/pep-0440/">PEP-440</a>')
 		}
 
 		stages {
