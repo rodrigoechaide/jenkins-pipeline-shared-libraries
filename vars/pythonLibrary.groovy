37c3918aa5ae50bb0470e78405bc065af9fd91c6
Rodrigo Echaide <rodrigoechaide@gmail.com>
2019-09-12 15:12:19 -0300

    Added RELEASE_VERSION and NEXT_DEV_VERSION params

--- vars/pythonLibrary.groovy
+++ vars/pythonLibrary.groovy
@@ -13,7 +13,9 @@ def call(Map pipelineParams) {
 
 		parameters {
 	    	booleanParam(name: 'RELEASE', defaultValue: false, description: 'Are you doing a Release?')
-		}		
+	    	string(name: 'RELEASE_VERSION', defaultValue: '', description: 'Version to be released (e.g: 1.1.0, 1.1.0rc1)')
+	    	string(name: 'NEX_DEV_VERSION', defaultValue: '', description: 'Next development version (e.g: 1.2.0.dev1) according to <a href="https://www.python.org/dev/peps/pep-0440/">PEP-440</a>')
+		}
 
 		stages {
 
@@ -64,7 +66,7 @@ def call(Map pipelineParams) {
 					ARTIFACT_REGISTRY_CREDENTIALS = credentials('73529b15-34f4-4912-9ef6-0829547c9586')
 				}
 				steps {			
-					sh 'make -C . -f /inc/release-me-python/python-release-with-params.mk pre-release upload-to-nexus post-release RELEASE_VERSION=$RELEASE_VERSION NEXT_DEVELOPMENT_VERSION=$NEXT_DEV_VERSION'
+					sh "make -C . -f /inc/release-me-python/python-release-with-params.mk pre-release upload-to-nexus post-release RELEASE_VERSION=${RELEASE_VERSION} NEXT_DEVELOPMENT_VERSION=${NEXT_DEV_VERSION}"
 				}
 			}
 		}
