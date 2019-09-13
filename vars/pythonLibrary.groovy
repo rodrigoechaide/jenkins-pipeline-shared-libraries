705fd3996a17bd1f13c326ecf754afbcf528482e
Rodrigo Echaide <rechaide@ascentio.com.ar>
2019-09-13 08:22:00 -0300

    Update pythonLibrary.groovy

--- vars/pythonLibrary.groovy
+++ vars/pythonLibrary.groovy
@@ -66,7 +66,7 @@ def call(Map pipelineParams) {
 					ARTIFACT_REGISTRY_CREDENTIALS = credentials('73529b15-34f4-4912-9ef6-0829547c9586')
 				}
 				steps {
-					sh "git config --local --add core.sshCommand 'ssh -i ~/.ssh/id_rsa'"
+					//sh "git config --local --add core.sshCommand 'ssh -i ~/.ssh/id_rsa'"
 					sh "make -C . -f /inc/release-me-python/python-release-with-params.mk pre-release upload-to-nexus post-release RELEASE_VERSION=${params.RELEASE_VERSION} NEXT_DEVELOPMENT_VERSION=${params.NEXT_DEV_VERSION}"
 				}
 			}
