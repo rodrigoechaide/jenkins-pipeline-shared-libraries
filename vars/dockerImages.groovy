2f5dbb8c59f6c224b4d127120d91c03dbd7dd35c
Rodrigo Echaide <rodrigoechaide@gmail.com>
2019-09-20 15:40:57 -0300

    Added release image steps

--- vars/dockerImages.groovy
+++ vars/dockerImages.groovy
@@ -41,7 +41,9 @@ def call(Map pipelineParams) {
 					expression { params.RELEASE == false }
 					}
 				steps {
-					sh "make push-image INTERNAL_REGISTRY_URL=${pipelineParams.dockerRegistrySnapshots}"
+					//Read value of docker version file and use that value to build the
+					sh "make push-image IMAGE_VERSION=\$(cat DOCKER_VERSION) INTERNAL_REGISTRY_URL=${pipelineParams.dockerRegistrySnapshots}"
+					sh "make push-image IMAGE_VERSION=latest INTERNAL_REGISTRY_URL=${pipelineParams.dockerRegistrySnapshots}"
 				}
 			}
 
@@ -50,13 +52,30 @@ def call(Map pipelineParams) {
 					expression { params.RELEASE == true }
 					}
 				environment {
-					//ARTIFACT_REGISTRY_URL = "${pipelineParams.artifactRegistryReleases}"
-					//ARTIFACT_REGISTRY_CREDENTIALS = credentials('73529b15-34f4-4912-9ef6-0829547c9586')
-					//SSH_KEY_CREDENTIALS = credentials('651c7382-f7d9-41a5-93ab-a6e197ee1d77')
-					//GIT_SSH_COMMAND="ssh -i ${SSH_KEY_CREDENTIALS} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+					ARTIFACT_REGISTRY_URL = "${pipelineParams.artifactRegistryReleases}"
+					ARTIFACT_REGISTRY_CREDENTIALS = credentials('73529b15-34f4-4912-9ef6-0829547c9586')
+					SSH_KEY_CREDENTIALS = credentials('651c7382-f7d9-41a5-93ab-a6e197ee1d77')
+					GIT_SSH_COMMAND="ssh -i ${SSH_KEY_CREDENTIALS} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 				}
 				steps {
-					sh "make push-image IMAGE_VERSION=${params.RELEASE_VERSION} INTERNAL_REGISTRY_URL=${pipelineParams.dockerRegistryReleases}"
+					echo "Doing Release Steps"
+					sh "mv IMAGE_VERSION IMAGE_VERSION_OLD"
+					sh "echo ${params.RELEASE_VERSION} > IMAGE_VERSION"
+					sh "git add IMAGE_VERSION"
+					sh "git tag -a ${params.RELEASE_VERSION} -m \"Created by Jenkins Sabiamar\""
+					sh "git commit -m \"Bump version: \$(cat IMAGE_VERSION_OLD) → ${params.RELEASE_VERSION}\""
+					sh "git push origin master"
+					sh "git push origin ${params.RELEASE_VERSION}"
+					echo "Pushing Image to CMS Registry"
+					sh "make push-image IMAGE_VERSION=${params.RELEASE_VERSION} INTERNAL_REGISTRY_URL=registry-cms.ascentio.com.ar" //Make sure login to the cms registry before pushing image
+					echo "Next Dev Version: Doing Bump Version Steps"
+					sh "echo ${params.NEXT_DEV_VERSION} > IMAGE_VERSION"
+					sh "git add IMAGE_VERSION"
+					sh "git commit -m \"Bump version: ${params.RELEASE_VERSION} → ${params.NEXT_DEV_VERSION}\""
+					sh "git push origin master"
+					echo "Pushing Image to DEV Registry"
+					sh "make push-image IMAGE_VERSION=${params.NEXT_DEV_VERSION} INTERNAL_REGISTRY_URL=nexus.ascentio.com.ar:7443"
+					sh "make push-image IMAGE_VERSION=latest INTERNAL_REGISTRY_URL=nexus.ascentio.com.ar:7443"
 				}
 			}
 		}
