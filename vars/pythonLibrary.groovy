ed5a28a4dba571d8496cc17964115f379eec0d8a
Rodrigo Echaide <rechaide@ascentio.com.ar>
2020-01-03 12:06:40 -0300

    Python unit pipeline

--- vars/pythonLibrary.groovy
+++ vars/pythonLibrary.groovy
@@ -7,6 +7,7 @@ def call(Map pipelineParams) {
 		agent {
 			dockerfile {
 				args '-v /home/administrator:/home/administrator -e HOME=/home/administrator'
+				filename "${pipelineParams.buildDockerfile}"
 			}
 		}
 
@@ -27,15 +28,29 @@ def call(Map pipelineParams) {
 				}
 			}
 
+			stage('Checking out Auxiliar Project') {
+				steps {
+					dir('inc') {
+						dir('release-me-python') {
+							echo "Checking out release-me-python project"
+							git(url: 'git@gitlab.ascentio.com.ar:asc-comp/release-me-python/release-me-python.git', 
+								branch: 'master',
+								credentialsId: '651c7382-f7d9-41a5-93ab-a6e197ee1d77')
+						}
+					}
+					echo 'release-me-python project succesfully checked out!'
+				}
+			}
+
 			stage('Unit-Tests') {
 				steps {
-					sh 'make -C . -f /inc/release-me-python/python-release-with-params.mk clean dist test'
+					sh 'make -C . -f inc/release-me-python/python-release-with-params.mk clean dist test'
 				}
 			}
 
 			stage('Project-Lint') {
 				steps {
-					sh "make -C . -f /inc/release-me-python/python-release-with-params.mk static-analysis MAIN_DIR=${pipelineParams.srcDir} TESTS_DIR=${pipelineParams.testDir}"
+					sh "make -C . -f inc/release-me-python/python-release-with-params.mk static-analysis MAIN_DIR=${pipelineParams.srcDir} TESTS_DIR=${pipelineParams.testDir}"
 				}
 			}
 
@@ -64,8 +79,8 @@ def call(Map pipelineParams) {
 					ARTIFACT_REGISTRY_CREDENTIALS = credentials('73529b15-34f4-4912-9ef6-0829547c9586')
 				}
 				steps {
-					echo 'Releasing snapshot version of the library'
-					sh 'make -C . -f /inc/release-me-python/python-release-with-params.mk upload-to-nexus'
+					echo 'Releasing snapshot version of the library'			
+					sh 'make -C . -f inc/release-me-python/python-release-with-params.mk upload-to-nexus'
 				}
 			}
 
@@ -81,7 +96,7 @@ def call(Map pipelineParams) {
 				}
 				steps {
 					echo 'Releasing new version of the library'
-					sh "make -C . -f /inc/release-me-python/python-release-with-params.mk pre-release upload-to-nexus post-release RELEASE_VERSION=${params.RELEASE_VERSION} NEXT_DEVELOPMENT_VERSION=${params.NEXT_DEV_VERSION}"
+					sh "make -C . -f inc/release-me-python/python-release-with-params.mk pre-release upload-to-nexus post-release RELEASE_VERSION=${params.RELEASE_VERSION} NEXT_DEVELOPMENT_VERSION=${params.NEXT_DEV_VERSION}"
 				}
 			}
 		}	
